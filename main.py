d5b303a5397fbd81940ba2fb7c7fd3cf88fa7b5c
NoahB6JS <noah.js.b6@gmail.com>
2025-12-17 10:13:25 +0000

    returning random quote
    when the button is pressed a random quote from the txt file is printed to the screen.
    
    only feature needed now is GUI to make it look nicer

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
 
-@app.route('/')
+def choosing_quote():
+    with open("quotes.txt", "r") as file:
+        quotes = [q.strip() for q in file if q.strip()]
+    return random.choice(quotes)
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/quote")
+def quote():
+    return choosing_quote()
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
-def choosing_quote():
-    with open("quotes.txt", "r") as file:
-        quotes = file.readlines()
-    return random.choice(quotes).strip()
